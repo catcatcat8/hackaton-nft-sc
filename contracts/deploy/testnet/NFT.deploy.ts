b08b0fcdbcaca80d61ed752328de2000eb911e39
evgeniy_dev_gotbit <evgeniy.lebedev@gotbit.io>
2024-10-16 19:07:05 +0300

    prettier

--- contracts/deploy/testnet/NFT.deploy.ts
+++ contracts/deploy/testnet/NFT.deploy.ts
@@ -3,18 +3,22 @@ import { DeployFunction } from 'hardhat-deploy/types'
 
 export const DEFAULT_COLLECTION_NAME = 'Крипто$лоня₽ы Collection'
 export const DEFAULT_COLLECTION_SYMBOL = 'KC'
-export const DEFAULT_BASE_URI = 'https://ipfs.io/ipfs/' 
+export const DEFAULT_BASE_URI = 'https://ipfs.io/ipfs/'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
   const { deploy } = deployments
-  
-  const {deployer} = await getNamedAccounts()
-  
+
+  const { deployer } = await getNamedAccounts()
+
   await deploy('NFT', {
     contract: 'NFT',
     from: deployer,
-    args: [DEFAULT_COLLECTION_NAME, DEFAULT_COLLECTION_SYMBOL, DEFAULT_BASE_URI],
+    args: [
+      DEFAULT_COLLECTION_NAME,
+      DEFAULT_COLLECTION_SYMBOL,
+      DEFAULT_BASE_URI,
+    ],
     log: true,
   })
 }
