1f77a6ab1a4f4ff20d317e47a95e7826ad823668
evgeniy_dev_gotbit <evgeniy.lebedev@gotbit.io>
2024-10-25 04:44:34 +0300

    new contract addr

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -11,7 +11,7 @@ export const ALLOWED_CHAIN_ID = 97
 export const SCANNER_LINK = 'https://testnet.bscscan.com/tx/'
 export const IPFS_BASE_LINK = 'https://ipfs.io/ipfs/'
 
-export const NFT_ADDR = '0x225774cB32E49bceA6Ac1F44F86cCE87ACd241b6'
+export const NFT_ADDR = '0x6ec6e3Ec075267c442aD6A8922B9128aF4C67d5F'
 export const NFT_CONTRACT = NFT__factory.connect(NFT_ADDR, DEFAULT_PROVIDER)
 
 export const PINATA_JWT = process.env.REACT_APP_PINATA_JWT ?? ''
